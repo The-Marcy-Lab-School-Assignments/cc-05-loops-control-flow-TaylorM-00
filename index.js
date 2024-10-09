276b50c87019ecb8053d92bd77ed89e4eeb2474a
Taylor <taybelle2020@gmail.com>
2024-10-09 11:13:21 -0400

    question 1 - 2 done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const multiplesOfSixAndNine = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 6 === 0 && i % 9 === 0) {
+      console.log(i);
+    }
+  }
+  return "done";
+};
+console.log(multiplesOfSixAndNine());
 
+const greaterNum = (num1, num2) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    if (num1 === num2) {
+      return "both integers are equal";
+    } else if (num1 > num2) {
+      return num1;
+    } else {
+      return num2;
+    }
+  } else {
+    return "Null";
+  }
+};
